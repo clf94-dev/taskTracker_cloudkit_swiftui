5c1a948dd209a4cd27fcd7431557d5c9cb982393
clf94-dev <clf94@hotmail.es>
2023-10-01 08:13:50 +0200

    - add watchos target - show list of tasks

---
+++ TaskTrackerWatch Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TaskTrackerWatch Watch App
+//
+//  Created by Carmen Lucas on 30/9/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TaskTrackerWatch Watch App/TaskTrackerWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskTrackerWatchApp.swift
+//  TaskTrackerWatch Watch App
+//
+//  Created by Carmen Lucas on 30/9/23.
+//
+
+import SwiftUI
+
+@main
+struct TaskTrackerWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
